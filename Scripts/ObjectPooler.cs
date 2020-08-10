2df1d1252f45ec0464f3b61d252118bdf6832abe
Keith Glazewski <moomoocowdeluxe@gmail.com>
2020-08-10 13:50:46 -0400

    Create base object pooler script

---
+++ Scripts/ObjectPooler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KeithComet.Pooling
+{
+    public class ObjectPooler : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
