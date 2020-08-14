ed668226d454844e19ea973eed816528a67c7407
Keith Glazewski <moomoocowdeluxe@gmail.com>
2020-08-14 08:29:08 -0400

    Add namespace to ObjectPool

--- Scripts/ObjectPool.cs
+++ Scripts/ObjectPool.cs
@@ -2,82 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
+namespace KeithComet.Pooling
 {
-    public const int PRELOADED_OBJECTS = 0;
-
-    private GameObject pooledPrefab;
-    private Transform activeContainer, inactiveContainer;
-    private List<GameObject> activeObjects, availableObjects;
-
-    public void Initialize(GameObject prefab, int objectsToPreload = PRELOADED_OBJECTS)
+    public class ObjectPool : MonoBehaviour
     {
-        pooledPrefab = prefab;
-        activeObjects = new List<GameObject>();
-        availableObjects = new List<GameObject>();
-        activeContainer = new GameObject("Active").transform;
-        inactiveContainer = new GameObject("Inactive").transform;
-        activeContainer.SetParent(transform);
-        inactiveContainer.SetParent(transform);
+        public const int PRELOADED_OBJECTS = 0;
 
-        for (int i = 0; i < objectsToPreload; i++)
-        {
-            GetObjectFromPool();
-        }
-    }
+        private GameObject pooledPrefab;
+        private Transform activeContainer, inactiveContainer;
+        private List<GameObject> activeObjects, availableObjects;
 
-    /// <summary>
-    /// Returns an available (inactive) object, or creates one if none available
-    /// </summary>
-    public GameObject GetObjectFromPool()
-    {
-        GameObject pooledObject;
-        //If there is an available (currently unused) object, grab it
-        if (availableObjects.Count > 0)
+        public void Initialize(GameObject prefab, int objectsToPreload = PRELOADED_OBJECTS)
         {
-            pooledObject = availableObjects[0];
-            availableObjects.Remove(pooledObject);
-            //Debug.Log("grabbed an available object. Remaining: " + availableObjects.Count);
-        }
-        else
-        {
-            //Otherwise, generate a new instance of the object
-            pooledObject = Instantiate(pooledPrefab);
-            //Debug.Log("no available objects. Object created");
-        }
-        pooledObject.SetActive(true);
-        pooledObject.transform.SetParent(activeContainer);
-        activeObjects.Add(pooledObject);
-        return pooledObject;
-    }
+            pooledPrefab = prefab;
+            activeObjects = new List<GameObject>();
+            availableObjects = new List<GameObject>();
+            activeContainer = new GameObject("Active").transform;
+            inactiveContainer = new GameObject("Inactive").transform;
+            activeContainer.SetParent(transform);
+            inactiveContainer.SetParent(transform);
 
-    public GameObject GetActiveObject(ObjectPool objectsPool, int index)
-    {
-        if (activeObjects.Count <= index)
-        {
-            Debug.Log("No active " + objectsPool.ToString() + " at " + index);
-            return null;
+            for (int i = 0; i < objectsToPreload; i++)
+            {
+                GetObjectFromPool();
+            }
         }
-        return activeObjects[index];
-    }
 
-    public void DeactivateObject(GameObject pooledObject)
-    {
-        pooledObject.SetActive(false);
-        pooledObject.transform.SetParent(inactiveContainer);
-        activeObjects.Remove(pooledObject);
-        if (!availableObjects.Contains(pooledObject))
-            availableObjects.Add(pooledObject);
-    }
-
-    public void DeactivateObjects(ObjectPool objectsPool)
-    {
-        for (int i = 0; i < activeObjects.Count; i++)
+        /// <summary>
+        /// Returns an available (inactive) object, or creates one if none available
+        /// </summary>
+        public GameObject GetObjectFromPool()
         {
-            activeObjects[i].SetActive(false);
-            activeObjects[i].transform.SetParent(inactiveContainer);
+            GameObject pooledObject;
+            //If there is an available (currently unused) object, grab it
+            if (availableObjects.Count > 0)
+            {
+                pooledObject = availableObjects[0];
+                availableObjects.Remove(pooledObject);
+                //Debug.Log("grabbed an available object. Remaining: " + availableObjects.Count);
+            }
+            else
+            {
+                //Otherwise, generate a new instance of the object
+                pooledObject = Instantiate(pooledPrefab);
+                //Debug.Log("no available objects. Object created");
+            }
+            pooledObject.SetActive(true);
+            pooledObject.transform.SetParent(activeContainer);
+            activeObjects.Add(pooledObject);
+            return pooledObject;
         }
-        availableObjects.AddRange(activeObjects);
-        activeObjects.Clear();
+
+        public GameObject GetActiveObject(ObjectPool objectsPool, int index)
+        {
+            if (activeObjects.Count <= index)
+            {
+                Debug.Log("No active " + objectsPool.ToString() + " at " + index);
+                return null;
+            }
+            return activeObjects[index];
+        }
+
+        public void DeactivateObject(GameObject pooledObject)
+        {
+            pooledObject.SetActive(false);
+            pooledObject.transform.SetParent(inactiveContainer);
+            activeObjects.Remove(pooledObject);
+            if (!availableObjects.Contains(pooledObject))
+                availableObjects.Add(pooledObject);
+        }
+
+        public void DeactivateObjects(ObjectPool objectsPool)
+        {
+            for (int i = 0; i < activeObjects.Count; i++)
+            {
+                activeObjects[i].SetActive(false);
+                activeObjects[i].transform.SetParent(inactiveContainer);
+            }
+            availableObjects.AddRange(activeObjects);
+            activeObjects.Clear();
+        }
     }
 }
