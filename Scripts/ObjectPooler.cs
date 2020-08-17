921fc626b2b40c57534e1b218097b9abf69dad4e
Keith Glazewski <moomoocowdeluxe@gmail.com>
2020-08-17 08:52:21 -0400

    Convert ObjectPooler to MonoBehaviour for simpler use

--- Scripts/ObjectPooler.cs
+++ Scripts/ObjectPooler.cs
@@ -6,14 +6,50 @@ namespace KeithComet.Pooling
 {
     public class ObjectPooler : MonoBehaviour
     {
-        void Start()
-        {
 
+        [SerializeField]
+        private PrefabPoolContainer prefabPoolContainer;
+
+        private static Dictionary<string, GameObject> prefabPool;
+        private static Dictionary<string, ObjectPool> objectPools = new Dictionary<string, ObjectPool>();
+
+        private static Transform objectPoolContainer;
+
+        private void Awake()
+        {
+            objectPoolContainer = transform;
+            bool provided = providePrefabPool(prefabPoolContainer.GetDictionary());
+            Debug.Log("Prefab pool provided: " + provided);
         }
 
-        void Update()
+        private static bool providePrefabPool(Dictionary<string, GameObject> providedPool)
         {
+            if(prefabPool != null)
+            {
+                objectPools = new Dictionary<string, ObjectPool>();
+                Debug.Log("Resetting Object Pools");
+            }
+            prefabPool = providedPool;
+            return true;
+        }
 
+        public static GameObject GetObjectFromPool(string poolKey)
+        {
+            if(!objectPools.ContainsKey(poolKey))
+            {
+                objectPools.Add(poolKey, createObjectPool(poolKey));
+            }
+            Debug.Log("Returning object from pool: " + poolKey);
+            return objectPools[poolKey].GetObjectFromPool();
+        }
+
+        private static ObjectPool createObjectPool(string poolKey)
+        {
+            GameObject newPoolObject = new GameObject(poolKey + " Pool", typeof(ObjectPool));
+            newPoolObject.transform.SetParent(objectPoolContainer);
+            ObjectPool newPool = newPoolObject.GetComponent<ObjectPool>();
+            newPool.Initialize(prefabPool[poolKey]);
+            return newPool;
         }
     }
 }
